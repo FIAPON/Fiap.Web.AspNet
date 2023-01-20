ca8f30b63bec9b16ab0cb5a2664fb6d4bea9863b
profflaviomoreni <profflavio.moreni@fiap.com.br>
2023-01-19 23:34:34 -0300

    funcionalidade de cliente e representante

--- Fiap.Web.AspNet/Models/ClienteModel.cs
+++ Fiap.Web.AspNet/Models/ClienteModel.cs
@@ -4,7 +4,6 @@
     {
         public int ClienteId { get; set; }
         public string? Nome { get; set; }
-        public string? Sobrenome { get; set; }
         public string? Email { get; set; }
         public DateTime DataNascimento { get; set; }
         public string? Observacao { get; set; }
